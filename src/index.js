b3dadda06c6652d3cddeef39211e8dbd7a4b0c44
shylton <mattas@oregonstate.edu>
2021-01-02 11:10:17 -0300

    completed game, with history and all

--- src/index.js
+++ src/index.js
@@ -65,7 +65,7 @@ class Game extends React.Component {
             xIsNext: (step % 2) === 0,
         });
     }
-    
+
     handleClick(i) {
         // save history up to this point, in case of trace backs
         const history = this.state.history.slice(0, this.state.stepNumber + 1);
@@ -112,7 +112,7 @@ class Game extends React.Component {
         } else {
             status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
         }
-        
+
         return (
             <div className="game">
                 <div className="game-board">
@@ -122,8 +122,8 @@ class Game extends React.Component {
                     />
                 </div>
                 <div className="game-info">
-                    <div>{ status }</div>
-                    <ol>{ moves }</ol>
+                    <div>{status}</div>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
@@ -131,23 +131,23 @@ class Game extends React.Component {
 }
 
 function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
     }
-  }
-  return null;
+    return null;
 }
 
 // ========================================
